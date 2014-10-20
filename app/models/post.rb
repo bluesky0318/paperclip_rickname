d2a90a25a10f90e010fc2a0da8770c2e484ff744
bluesky0318 <42303596@qq.com>
2014-10-20 14:15:04 +0800

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ActiveRecord::Base
   
   has_attached_file :attachment#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   # Validate content type
-  validates_attachment_content_type :attachment, :content_type => ["application/octet-stream"]
+  validates_attachment_content_type :attachment, :content_type => ["application/x-zip-compressed"]
   # Validate filename
-  validates_attachment_file_name :attachment, :matches => [/oce\Z/, /OCE\Z/]
+  validates_attachment_file_name :attachment, :matches => [/zip\Z/, /ZIP\Z/]
   # Explicitly do not validate
   #do_not_validate_attachment_file_type :attachment
 end
